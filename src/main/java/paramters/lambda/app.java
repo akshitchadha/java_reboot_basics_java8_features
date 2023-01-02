9ae4eda2e40ea3282b559aa366059cc926ca0fe8
akshitchadha <achadha1995@gmail.com>
2023-01-02 22:48:09 +0530

    parameterised lambda implementation

---
+++ src/main/java/paramters/lambda/app.java
@@ -0,0 +1,11 @@
+package paramters.lambda;
+
+public class app {
+    public static void main(String[] args) {
+
+        sum s=(int a, int b)-> System.out.print(a+b);
+
+        s.add(1,2);
+
+            }
+}
---
+++ src/main/java/paramters/lambda/sum.java
@@ -0,0 +1,7 @@
+package paramters.lambda;
+
+@FunctionalInterface
+public interface sum {
+
+    void add(int a,int b);
+}
