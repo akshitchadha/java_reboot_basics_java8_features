baf027294ee2ed4013e965a94550f562a83c8d77
akshitchadha <achadha1995@gmail.com>
2023-01-07 16:21:38 +0530

    passing lambda as argument or pass behaviour as agrument and joins in predicate

--- src/main/java/Default/methods/diamondproblem/B.java
+++ src/main/java/Default/methods/diamondproblem/B.java
@@ -1,10 +1,10 @@
 package Default.methods.diamondproblem;
 
 public class B implements A,X {
-//    @Override
-//    public void methodM1() {
-//
-//        System.out.println("inside class B");
-//
-//    }
+    @Override
+    public void methodM1() {
+
+        System.out.println("inside class B");
+
+    }
 }
